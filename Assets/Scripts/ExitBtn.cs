e83db9915fe3cf9e4ca04d3bb4daf92805660947
Greg Solo <gregory.soloshchenko@gmail.com>
2021-08-12 23:39:32 -0400

    Menu scene contains Exit button that quits the game

---
+++ Assets/Scripts/ExitBtn.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class ExitBtn : MonoBehaviour
+    {
+        public void OnClick()
+        {
+            Application.Quit();
+        }
+    }
+}
